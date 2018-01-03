552d63d2e08eac61d4536cf467bc3a3855f86aa9
Caroline Wanjiku <Caroline Wanjiku>
2018-01-03 16:07:19 +0300

    ft(tests)Add failing tests - Add tests for the methods. - Tests should fail

--- tests/tests_stringclass.js
+++ tests/tests_stringclass.js
@@ -1,31 +1,77 @@
-import { stringClass } from '../src/stringclass'
 
+describe('the hasVowels method returns true if the string contains vowels', function(){
+    it('returns true if vowel found', function(){});
+    it('returns message if no vowel found', function(){});
+});
 
-describe('The hasVowel method returns true', function(){});
+describe('The toUpper method converts a string to uppercase', function(){
+    it('returns capital if conversion successful', function(){});
+    it('returns message if string was already in uppercase', function(){});
+});
 
-describe('The toUpper method converts a string to uppercase', function(){});
+describe('The toLower method converts a string to lowercase', function(){
+    it('returns lowercase string if conversion successful', function(){});
+    it('returns message if string was already in lowercase', function(){});
+});
 
-describe('The toLower method converts a string to lowercase', function(){});
+describe('ucFirst converts the first letter of a string to uppercase', function(){
+    it('returns true if conversion successful', function(){});
+    it('returns message if first letter was already uppercase', function(){});
+});
 
-describe('ucFirst converts the first letter of a string to uppercase', function(){});
+describe('The isQuestion returns true if string is a question', function(){
+    it('returns true string is question', function(){});
+    it('returns message if string not question', function(){});
+});
 
-describe('The isQuestion returns true if string is a question', function(){});
+describe('The in method returns a list of the words in the string, as an array', function(){
+    it('returns true if string returned as array', function(){});
+    it('returns message if string empty', function(){});
+});
 
-describe('The in method returns a list of the words in the string, as an array', function(){});
+describe('The wordCount method returns the number of words in the string', function(){
+    it('returns true when word count is returned', function(){});
+    it('asserts that wordcount is correct', function(){});
+});
 
-describe('The wordCount method returns the number of words in the string', function(){});
+describe('The toCurrency method returns a currency representation of the String', function(){
+    it('returns message if string empty', function(){});
+    it('returns message if string not a number', function(){});
+    it('returns true if string is converted to currency form', function(){});
+});
 
-describe('The toCurrency method returns a currency representation of the String', function(){});
-
-describe('The fromCurrency method returns a number representation of the Currency String', function(){});
+describe('The fromCurrency method returns a number representation of the Currency String',
+function(){
+    it('returns message if string empty', function(){});
+    it('returns message if string is not number', function(){});
+    it('returns true if number representation returned', function(){});
+});
 
 describe('The inverseCase method returns each letter in the string as an inverse of its current case', 
-function(){});
+function(){
+    it('returns message if string empty', function(){});
+    it('returns true if case inverted', function(){});
+});
 
-describe('The alternateCase method returns the letters in alternating cases', function(){});
+describe('The alternateCase method returns the letters in alternating cases', function(){
+    it('returns message if string empty', function(){});
+    it('returns true of alternated string returned', function(){});
+});
 
-describe('The getMiddle method returns the character(s) in the middle of the string', function(){});
+describe('The getMiddle method returns the character(s) in the middle of the string', function(){
+    it('returns message if string empty', function(){});
+    it('returns true if character in middle returned', function(){});
+});
 
-describe('The numberWords method Rreturns the numbers in words', function(){});
+describe('The numberWords method returns the numbers in words', function(){
+    it('returns message if string empty', function(){});
+    it('returns message if string is not a number', function(){});
+    it('returns true if number converted to words', function(){});
+});
 
-describe('The isDigit method returns true if the string is a digit', function(){});
+describe('The isDigit method returns true if the string is a digit', function(){
+    it('returns message if string empty', function(){});
+    it('returns message if string not a number', function(){});
+    it('returns true if string is a single digit', function(){});
+    it('returns false string is more than one digit', function(){});
+});
